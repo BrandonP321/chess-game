cf6a992b6c5eff0fb323e4e6e74eb2685dafb82c
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-23 20:27:03 -0800

    user can now join an existing room

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,29 +4,43 @@ import socketIOClient from 'socket.io-client'
 const ENDPOINT = 'http://localhost:8000'
 
 export default function Home() {
+    
     let socket;
 
     // on load, connect to socket server
     useEffect(() => {
         socket = socketIOClient(ENDPOINT)
-
+        
         socket.on('connect', () => {
             console.log('connected')
         })
-
+        
         socket.on('newRoomCreated', room => {
             console.log('created room ' + room)
             // redirect to game room with room's id
             window.location.href = '/game/' + room
         })
+        
+        socket.on('allowRoomJoin', room => {
+            // redirect to the game with the given room id
+            window.location.href = '/game/' + room
+        })
+
     }, [])
 
     const handleNewRoomCreate = () => {
+        // tell socket.io server to make a new room for the user
         socket.emit('createNewRoom')
     }
 
-    const handleAttemptRoomJoin = () => {
+    const handleAttemptRoomJoin = (event) => {
+        event.preventDefault();
+        
+        // roomid from input field
+        const roomId = event.target.children[0].value
 
+        // tell socket.io server to check for a room with the given id
+        socket.emit('joinExistingRoom', roomId)
     }
 
     return (
